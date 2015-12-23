943763b0870d6e958856fbe7b7f77d584e4ac652
Mark Gray <markgray.online@googlemail.com>
2015-12-23 21:01:56 +0000

    Adding fsx files for 'Introducing map'

---
+++ PredictiveText/Maps.fsx
@@ -0,0 +1,31 @@
+﻿let empty = Map.empty
+
+let mapped = empty.Add("Key","Value")
+
+mapped.["Key"]
+mapped.["not there"]
+
+mapped.TryFind("not there")
+mapped |> Map.tryFind "Key"
+
+let selfPwrs = [for f in 1.0..7.0 do yield (f, f**f)] |> Map.ofList
+selfPwrs |> Map.tryFind 7.0
+
+let findPower x =
+    match selfPwrs |> Map.tryFind x with
+    | Some f -> printfn "%f" f
+    | None -> printfn "Number not found."
+
+let capitals =
+    [("Australia", "Canberra"); ("Canada", "Ottawa"); ("China", "Beijing");
+        ("Denmark", "Copenhagen"); ("Egypt", "Cairo"); ("Finland", "Helsinki");
+        ("France", "Paris"); ("Germany", "Berlin"); ("India", "New Delhi");
+        ("Japan", "Tokyo"); ("Mexico", "Mexico City"); ("Russia", "Moscow");
+        ("Slovenia", "Ljubljana"); ("Spain", "Madrid"); ("Sweden", "Stockholm");
+        ("Taiwan", "Taipei"); ("USA", "Washington D.C.")]
+    |> Map.ofList
+
+let findCapital country =
+    match capitals |> Map.tryFind country with
+    | Some cap -> printfn "The capital of %s is %s." country cap
+    | None -> printfn "Unknown country specified."
