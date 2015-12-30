a56772e0a79be9540b4c520eb37aa26c4f7a43b6
Mark Gray <markgray.online@googlemail.com>
2015-12-30 15:34:21 +0000

    Adding files for the recursive functions lecture

--- PredictiveText/Trie.fs
+++ PredictiveText/Trie.fs
@@ -5,4 +5,24 @@
         | IncompleteWord
 
     type Trie =
-        { flag : NodeFlag }
+        { children : Map<char, Trie>
+          flag : NodeFlag }
+
+    let emptyTrie =
+        { children = Map.empty
+          flag = IncompleteWord }
+
+    let insertWord word initialTrie =
+        let rec innerInsertChars charList currentTrie =
+            match charList with
+            | [] -> { emptyTrie with flag = EndOfWord }
+            | ch::rest ->
+                let innerMap = currentTrie.children
+                let innerTrie =
+                    match innerMap |> Map.tryFind ch with
+                    | None -> emptyTrie
+                    | Some childTrie -> childTrie
+                let newInnerTrie = innerInsertChars rest innerTrie
+                let newMap = innerMap |> Map.add ch newInnerTrie
+                { currentTrie with children = newMap }
+        innerInsertChars (word |> Seq.toList) initialTrie
