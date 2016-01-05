01531182789a1dc17e00c6ef0e911671b34d92ed
Mark Gray <markgray.online@googlemail.com>
2016-01-05 10:39:29 +0000

    Adding content for the Sets video

---
+++ PredictiveText/SetPlay.fsx
@@ -0,0 +1,13 @@
+﻿let empty = Set.empty
+
+let toBe = Set.ofList ["to";"be";"or";"not";"to";"be"]
+
+let that = Set.add "that" empty
+
+let thatQuestion = Set.ofList ["that";"is";"the";"question"]
+
+let thatToBeQuest = toBe |> Set.union that |> Set.union thatQuestion
+let thatToBeQuest2 = toBe + that + thatQuestion
+
+let toBeDiff = toBe |> Set.difference thatToBeQuest2
+let toBeDiff2 = thatToBeQuest2 - that
---
+++ PredictiveText/Soliloquy.fsx
@@ -0,0 +1,58 @@
+﻿let __COMPLETE_ME__<'T> : 'T = raise <| new System.NotImplementedException("You must implement this to continue.")
+
+open System.IO
+open System.Text
+open System.Text.RegularExpressions
+
+let words = 
+    File.ReadLines(Path.Combine(__SOURCE_DIRECTORY__, "soliloquy.txt")) // Make sure you save the text file in the source directory
+    |> Seq.collect // Using collect as the the fun will return an sequence of sequences, collect will flatten into a single sequence
+        (fun line -> 
+            seq 
+                { for matches in Regex("\w+").Matches(line) -> // the Regex splits the line into individual words
+                  matches.Value.ToUpper() }) // Use toUpper so that the words are the same
+     |> Seq.toList
+
+// Step 1: Verify the words in the test exist in the body of text that has been loaded, 
+// you can used List.contains. Then you can run the tests: "runTests()"
+let slings()   = words |> List.contains "SLINGS"
+let shuffled() = words |> List.contains "SHUFFLED"
+let brigand()  = words |> List.contains "BRIGAND"
+
+// Step 2: Build a "Set" from the "words" list
+let set() : Set<string> = Set.ofList words 
+
+// Step 3: Use "set" you just created instead of words directly, see if you can
+// find the rather *relevant* hidden word in the text after it has been mapped.
+// You can leave the indexes, assume they are already correct.
+let findMe = 
+    [44;119;50;5;108;98]
+    |> List.map (fun i -> (set() |> Set.toList).[i].[0], (set() |> Set.toList).[i]) 
+    |> List.map (fun (k,v) -> sprintf "[%c] - %s" k v)
+
+// Run these when your are ready
+let runTests() =
+    let areEquivalent description actual expected =
+        if actual <> expected
+        then failwith (sprintf "Test Failed: %A - %A is not equivalent to %A" description expected actual)
+        else printfn "Test Passed: %A" description
+
+    areEquivalent
+        "Should contain the word 'SLINGS'"
+        (slings())
+        (true)
+
+    areEquivalent
+        "Should contain the word 'SHUFFLED'"
+        (shuffled())
+        (true)
+
+    areEquivalent
+        "Should not contain the word 'BRIGAND'"
+        (brigand())
+        (false)
+
+    areEquivalent
+        "Should not contain 'MORE LIFE SAY BE PAUSE WE'"
+        (findMe = ["[M] - MORE"; "[L] - LIFE"; "[S] - SAY"; "[B] - BE"; "[P] - PAUSE"; "[W] - WE"])
+        (false)
